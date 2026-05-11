a5f6b97d3a1324cff69daeefbf82a6a6b6b10920
Anil Abhange <149065554+BinaryFighter-01@users.noreply.github.com>
2026-05-11 08:48:44 +0530

    Refactor genetic algorithm for neural network training

--- CI2.py
+++ CI2.py
@@ -1,52 +1,77 @@
+import warnings
+warnings.filterwarnings('ignore')
 import random
-from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-from sklearn.neural_network import MLPClassifier
+from sklearn.datasets import load_iris
 from sklearn.metrics import accuracy_score
+from sklearn.neural_network import MLPClassifier
 
 
-# Load data
-X, y = load_iris(return_X_y=True)
 
-# Split data
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2
-)
+data=load_iris()
+X=data.data
+y=data.target
 
-best_acc = 0
-best = None
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2)
 
-# Genetic Algorithm
-for i in range(5):
-
-    # Random parameters
-    neurons = random.randint(5, 20)
-
-    lr = random.uniform(0.001, 0.1)
-
-    # Neural Network
-    model = MLPClassifier(
-        hidden_layer_sizes=(neurons,),
-        learning_rate_init=lr,
-        max_iter=300
-    )
-
-    model.fit(X_train, y_train)
-
-    pred = model.predict(X_test)
-
-    acc = accuracy_score(y_test, pred)
-
-    print(neurons, lr, acc)
-
-    # Best solution
-    if acc > best_acc:
-
-        best_acc = acc
-
-        best = [neurons, lr]
+def create_population(size):
+    population=[]
+    for _ in range(size):
+        neurons=random.randint(5,20)
+        lr=random.uniform(0.001,0.1)
+        population.append([neurons,lr])
+    return population
 
 
-print("\nBest Parameters =", best)
+def fitness(individual):
+    neurons,lr=individual
+    model=MLPClassifier(hidden_layer_sizes=(neurons,),learning_rate_init=lr,max_iter=30)
 
-print("Best Accuracy =", best_acc)
+    model.fit(X_train,y_train)
+    predictions=model.predict(X_test)
+    return accuracy_score(y_test,predictions)
+    
+
+def select(population):
+    population.sort(key=fitness,reverse=True)
+    return population[:2]
+
+
+def crossover(parent1,parent2):
+    return [parent1[0],parent2[1]]
+
+
+def mutate(individual):
+    if random.random()<0.3:
+        individual[0]=random.randint(5,20)
+
+    if random.random()<0.3:
+        individual[1]=random.uniform(0.001,0.1)
+
+    return individual
+
+population=create_population(6)
+
+for generation in range(5):
+    best=select(population)
+    new_population=best.copy()
+    while(len(new_population)<6):
+        parent1=random.choice(best)
+        parent2=random.choice(best)
+
+        child=crossover(parent1,parent2)
+        child=mutate(child)
+        new_population.append(child)
+
+    population=new_population
+    
+
+best_ind=select(population)[0]
+print("best_ind",best_ind)
+
+final_model=MLPClassifier(hidden_layer_sizes=(best_ind[0],),learning_rate_init=best_ind[1],max_iter=30)
+
+
+final_model.fit(X_train,y_train)
+final_pred=final_model.predict(X_test)
+print("Final acc:",accuracy_score(y_test,final_pred))
