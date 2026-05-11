bee57092326b55fd02e03ceb693bcdd7f10924f1
Anil Abhange <149065554+BinaryFighter-01@users.noreply.github.com>
2026-05-11 08:16:24 +0530

    Update CI2.py

--- CI2.py
+++ CI2.py
@@ -1,32 +1,52 @@
+import random
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
-import random
+from sklearn.metrics import accuracy_score
+
 
 # Load data
 X, y = load_iris(return_X_y=True)
-Xtr, Xte, ytr, yte = train_test_split(X, y, test_size=0.2)
 
-# Create individual [neurons, learning_rate]
-def create():
-    return [random.randint(5, 50), random.uniform(0.001, 0.1)]
+# Split data
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2
+)
 
-# Fitness = accuracy
-def fit(ind):
-    m = MLPClassifier(hidden_layer_sizes=(ind[0],),
-                      learning_rate_init=ind[1],
-                      max_iter=200)
-    m.fit(Xtr, ytr)
-    return m.score(Xte, yte)
+best_acc = 0
+best = None
 
-# Initial population
-pop = [create() for _ in range(6)]
+# Genetic Algorithm
+for i in range(5):
 
-# GA loop
-for _ in range(5):
-    pop = sorted(pop, key=fit, reverse=True)[:2] + \
-          [create() for _ in range(4)]
+    # Random parameters
+    neurons = random.randint(5, 20)
 
-# Best result
-best = max(pop, key=fit)
-print(best, fit(best))
+    lr = random.uniform(0.001, 0.1)
+
+    # Neural Network
+    model = MLPClassifier(
+        hidden_layer_sizes=(neurons,),
+        learning_rate_init=lr,
+        max_iter=300
+    )
+
+    model.fit(X_train, y_train)
+
+    pred = model.predict(X_test)
+
+    acc = accuracy_score(y_test, pred)
+
+    print(neurons, lr, acc)
+
+    # Best solution
+    if acc > best_acc:
+
+        best_acc = acc
+
+        best = [neurons, lr]
+
+
+print("\nBest Parameters =", best)
+
+print("Best Accuracy =", best_acc)
