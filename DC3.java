7d68a1b6633c130f8aad97f1437de9fd6b87f440
Anil Abhange <149065554+BinaryFighter-01@users.noreply.github.com>
2026-05-11 07:54:26 +0530

    Implement load balancer to distribute requests

--- DC3.java
+++ DC3.java
@@ -1,1 +1,27 @@
+public class LoadBalancerLC{
+    public static void main(String[] args){
+        String servers[] = {
+            "Server 1",
+            "Server 2",
+            "Server 3"
+        };
 
+        int connections [] = {4,5,2};
+
+        for (int i = 1; i < 10;i++){
+            int min = 0;
+
+            for(int j = 1;j<connections.length;j++){
+                if(connections[j]<connections[min]){
+                    min = j;
+                }
+            }
+
+            System.out.println(
+                "Requests " + i + " is handled by " + servers[min]
+            );
+
+            connections[min]++;
+        }
+    }
+}
