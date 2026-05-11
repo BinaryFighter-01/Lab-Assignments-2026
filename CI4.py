8104b0c080e5cd85bff424c2451a391331702f03
Anil Abhange <149065554+BinaryFighter-01@users.noreply.github.com>
2026-05-11 08:20:58 +0530

    Refactor genetic algorithm implementation in CI4.py

--- CI4.py
+++ CI4.py
@@ -1,33 +1,32 @@
-from deap import base, creator, tools, algorithms
 import random
+from deap import base, creator, tools, algorithms
 
-# Maximize function: f(x) = x^2
-def eval_func(ind):
-    return (ind[0]**2,)
-
-# Define fitness and individual
+# 1. Fitness & Individual (ALWAYS FIRST)
 creator.create("FitnessMax", base.Fitness, weights=(1.0,))
 creator.create("Individual", list, fitness=creator.FitnessMax)
 
+# 2. Toolbox
 toolbox = base.Toolbox()
 
-# Gene: random number
-toolbox.register("attr", random.uniform, -10, 10)
-
-# Individual and population
-toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr, 1)
+toolbox.register("attr", random.randint, 0, 10)
+toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr, 3)
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
-# Operators
-toolbox.register("evaluate", eval_func)
-toolbox.register("mate", tools.cxBlend, alpha=0.5)
-toolbox.register("mutate", tools.mutGaussian, mu=0, sigma=1, indpb=0.2)
-toolbox.register("select", tools.selTournament, tournsize=3)
+# 3. Fitness function
+def eval(ind):
+    return (ind[0]**2,)   # ALWAYS tuple
 
-# Run GA
-pop = toolbox.population(n=10)
-algorithms.eaSimple(pop, toolbox, cxpb=0.5, mutpb=0.2, ngen=5, verbose=False)
+toolbox.register("evaluate", eval)
 
-# Best result
+# 4. Operators
+toolbox.register("mate", tools.cxTwoPoint)
+toolbox.register("mutate", tools.mutUniformInt, low=0, up=10, indpb=0.2)
+toolbox.register("select", tools.selBest)
+
+# 5. Run
+pop = toolbox.population(n=5)
+algorithms.eaSimple(pop, toolbox, cxpb=0.7, mutpb=0.2, ngen=5)
+
+# 6. Result
 best = tools.selBest(pop, 1)[0]
-print("Best:", best, "Fitness:", best.fitness.values)
+print(best, best.fitness.values)
